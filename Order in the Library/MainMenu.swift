932fbd20cd4ab38bdeca88cad1473964d02169ca
Aiden <aidenbak9@gmail.com>
2026-01-29 09:13:01 -0600

    Added main menu (temp)

---
+++ Order in the Library/MainMenu.swift
@@ -0,0 +1,18 @@
+//
+//  MainMenu.swift
+//  Order in the Library
+//
+//  Created by Aiden Baker on 1/29/26.
+//
+
+import SwiftUI
+
+struct MainMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainMenu()
+}
