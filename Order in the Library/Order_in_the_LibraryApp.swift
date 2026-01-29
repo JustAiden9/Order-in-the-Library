5520a1357e7249b31c14bca1c030d64afe26ac5b
Aiden <aidenbak9@gmail.com>
2026-01-29 09:11:14 -0600

    Initial Commit

---
+++ Order in the Library/Order_in_the_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Order_in_the_LibraryApp.swift
+//  Order in the Library
+//
+//  Created by Aiden Baker on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct Order_in_the_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Order in the LibraryTests/Order_in_the_LibraryTests.swift
@@ -0,0 +1,17 @@
+//
+//  Order_in_the_LibraryTests.swift
+//  Order in the LibraryTests
+//
+//  Created by Aiden Baker on 1/29/26.
+//
+
+import Testing
+@testable import Order_in_the_Library
+
+struct Order_in_the_LibraryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
